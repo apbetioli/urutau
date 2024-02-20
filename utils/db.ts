e3567dbda4751166da63e7fac8cd9f98dcfe9741
Alexandre Betioli <apbetioli@gmail.com>
2024-02-21 00:07:35 +0100

    Prisma configuration

---
+++ utils/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+// This is a workaround for next hot-reloading causing issues with the db connection when developing
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
