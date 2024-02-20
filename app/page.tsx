632e71198bd09fa6bd18e7807284d1b89c353aaf
Alexandre Betioli <apbetioli@gmail.com>
2024-02-20 22:11:25 +0100

    Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return <main>Hello</main>
+  return (
+    <main className="flex w-screen h-screen bg-black justify-center items-center text-white">
+      <div className="w-full max-w-xl m-auto">
+        <h1 className="text-6xl">Urutau</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          Unlimited bedtime stories at one click.
+        </p>
+        <div>
+          <button className="bg-purple-600 px-4 py-2 rounded-lg text-xl">
+            Get started
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
