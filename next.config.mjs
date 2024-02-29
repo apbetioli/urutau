50b218ff5b9a84915899cea0aa5334b16939cce0
Alexandre Betioli <apbetioli@gmail.com>
2024-02-29 16:24:11 +0100

    Fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: {
-      hostname: 'urutau-kappa.vercel.app',
-    },
+    remotePatterns: [
+      {
+        hostname: 'urutau-kappa.vercel.app',
+      },
+    ],
   },
 }
 
