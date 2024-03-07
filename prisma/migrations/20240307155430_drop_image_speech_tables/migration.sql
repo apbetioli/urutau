30c90a2aaa36bf9a424d104fe4463bb2298df5f2
Alexandre Betioli <apbetioli@gmail.com>
2024-03-07 16:55:32 +0100

    Drop image and speech tables

---
+++ prisma/migrations/20240307155430_drop_image_speech_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Image` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Speech` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Image";
+
+-- DropTable
+DROP TABLE "Speech";
