7e89e6fc0451f29161c6d55a0879a2ebf59abadc
Alexandre Betioli <apbetioli@gmail.com>
2024-03-13 18:53:50 +0100

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['urutau.s3.amazonaws.com'],
+    remotePatterns: [
+      {
+        hostname: 'urutau.s3.amazonaws.com',
+      },
+    ],
   },
 }
 
