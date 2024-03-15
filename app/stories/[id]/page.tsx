b7164cb8c0e2f87a559b2afabcf2dbc4f01f4cc9
Alexandre Betioli <apbetioli@gmail.com>
2024-03-15 14:46:05 +0100

    Block access to private story

--- app/stories/[id]/page.tsx
+++ app/stories/[id]/page.tsx
@@ -1,13 +1,12 @@
-import { Story } from '@prisma/client'
-
 import Button from '@/components/Button'
 import Empty from '@/components/Empty'
 import StoryDetail from '@/components/StoryDetail'
+import { getUserByClerkId } from '@/utils/server/auth'
 import { prisma } from '@/utils/server/db'
 import Link from 'next/link'
 
 const getStory = async (id: string) => {
-  return await prisma.story.findUnique({
+  const story = await prisma.story.findUniqueOrThrow({
     where: {
       id,
     },
@@ -19,6 +18,15 @@ const getStory = async (id: string) => {
       },
     },
   })
+
+  if (!story.public) {
+    const user = await getUserByClerkId()
+    if (story.userId !== user.id) {
+      throw new Error('Not found!')
+    }
+  }
+
+  return story
 }
 
 export default async function StoryPage({
@@ -26,7 +34,7 @@ export default async function StoryPage({
 }: {
   params: { id: string }
 }) {
-  const story = (await getStory(params.id)) as Story
+  const story = await getStory(params.id)
   return (
     <div className="flex flex-col w-full h-full">
       {story ? (
