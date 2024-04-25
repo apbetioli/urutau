aa38ca465c0219498f5dfbaf2521d5b797802585
Alexandre Betioli <apbetioli@gmail.com>
2024-04-25 22:43:53 +0200

    Organize imports

--- app/(main)/stories/page.tsx
+++ app/(main)/stories/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import Button from '@/components/Button'
 import Feed from '@/components/Feed'
 import NewStory from '@/components/NewStory'
 import { getStories } from '@/utils/api'
-import { SignedIn, SignedOut } from '@clerk/nextjs'
-import Link from 'next/link'
 
 export default function StoriesPage() {
   return (
